3cd562f1a8b3200db2d2ad5c4c35d4c9528f35ef
Adarsh Agrahara <67204808+boogeyman-is-back-at-crabfest@users.noreply.github.com>
2022-02-27 15:06:45 -0800

    Time: 0 ms (100.00%), Space: 39.9 MB (51.14%) - LeetHub

--- 1108-defanging-an-ip-address/1108-defanging-an-ip-address.java
+++ 1108-defanging-an-ip-address/1108-defanging-an-ip-address.java
@@ -1,6 +1,6 @@
 class Solution {
     public String defangIPaddr(String address) {
-        
-        return address.replace(".","[.]");
+        String ans = address.replace(".","[.]");
+        return ans;
     }
 }
