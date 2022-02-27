0d8179d227e6420849cec8bce9e5e109c9e567fb
Adarsh Agrahara <67204808+boogeyman-is-back-at-crabfest@users.noreply.github.com>
2022-02-26 23:42:32 -0800

    Time: 2 ms (67.40%), Space: 75.8 MB (45.93%) - LeetHub

---
+++ 53-maximum-subarray/53-maximum-subarray.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int maxSum = Integer.MIN_VALUE;
+        int runningTotal = 0;
+        for(int i : nums) {
+            runningTotal += i;
+            
+            if(runningTotal > maxSum) {
+                maxSum = runningTotal;
+            }
+            
+            if(runningTotal < 0) {
+                runningTotal = 0; //effectively resetting
+            }
+        }
+        
+        return maxSum;
+    }
+}
