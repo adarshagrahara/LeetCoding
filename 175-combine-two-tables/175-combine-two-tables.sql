0048fe17a666b48eb17252ed23c6b0700bf7861a
Adarsh Agrahara <67204808+boogeyman-is-back-at-crabfest@users.noreply.github.com>
2022-03-02 23:17:23 -0800

    Time: 647 ms (86.22%), Space: 0B (100.00%) - LeetHub

--- 175-combine-two-tables/175-combine-two-tables.sql
+++ 175-combine-two-tables/175-combine-two-tables.sql
@@ -1,2 +1,2 @@
-# Write your MySQL query statement below
+/* Write your PL/SQL query statement below */
 SELECT Person.FirstName, Person.LastName, Address.City, Address.State from Person LEFT JOIN Address on Person.PersonId = Address.PersonId;
