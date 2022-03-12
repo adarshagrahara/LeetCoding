b45fc96ef344340d7238a8c4b51257c47864dbb4
Adarsh Agrahara <67204808+boogeyman-is-back-at-crabfest@users.noreply.github.com>
2022-03-11 21:15:35 -0800

    Time: 1 ms (41.95%), Space: 46.2 MB (9.66%) - LeetHub

---
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.java
@@ -0,0 +1,57 @@
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+
+class Solution {
+    public Node copyRandomList(Node head) {
+       if (head == null) return null;
+        
+        HashMap<Node, Node> map = new HashMap<>();
+
+        Node newHead = new Node(head.val);
+        Node res = newHead;
+
+        while (head != null) {
+            map.put(head, newHead);
+            
+            if (head.next != null ) {
+                Node mapNext = map.get(head.next);
+                if(mapNext != null) {
+                    newHead.next = mapNext;
+                } else {
+                    Node newNext = new Node(head.next.val);
+                    newHead.next = newNext;
+                    map.put(head.next, newNext);
+                }
+            }
+            
+            if (head.random != null) {
+                Node mapRandom = map.get(head.random);
+                if(mapRandom != null) {
+                    newHead.random = mapRandom;
+                } else {
+                    Node newRandom = new Node(head.random.val);
+                    newHead.random = newRandom;
+                    map.put(head.random, newRandom);
+                }
+                                
+            }
+            
+            head = head.next;
+            newHead = newHead.next;
+        }
+        
+        return res;
+    }
+}
